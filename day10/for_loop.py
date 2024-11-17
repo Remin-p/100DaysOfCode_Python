129f0bd25d0d1de5b0c94123241fe534783ec384
remin-p <remin-p@proton.me>
2024-11-17 15:58:46 -0300

    day 10

---
+++ day10/for_loop.py
@@ -0,0 +1,12 @@
+'''
+Day 10 - For-loop
+Use a for loop to print numbers from 1 to 10.
+
+USER: If I wasn't supposed to know about this then, how could I have done any of the other ones I did previously?
+'''
+print("For loop...")
+
+for i in range(10):
+    print(i+1)
+
+# Easy peasy.
