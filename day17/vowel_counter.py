d39810d13b88ea2bce381a0debde380c321e6338
remin-p <remin-p@proton.me>
2024-11-21 20:16:12 -0300

    day 17: String vowel counter

---
+++ day17/vowel_counter.py
@@ -0,0 +1,19 @@
+"""
+Day 17 - Number of vowels in a string
+Write a function to count the number of vowels in a string.
+"""
+
+print("Vowel counter...")
+
+vowels = ["a", "e", "i", "o", "u"]
+
+
+def vowelCounter(userStr):
+    vowelsInStr = 0
+    for char in userStr:
+        if char in vowels:
+            vowelsInStr += 1
+    return vowelsInStr
+
+
+print(vowelCounter(input("Insert your string: ")))  # wacky
